87a6f61656018d72b866abddd04cff63333bda6f
kiweezi <rhysmjones2308'gmail.com>
2021-01-08 16:54:54 +0000

    Add basic script structure and comments

---
+++ bot.py
@@ -0,0 +1,27 @@
+# A bot which notifies the user of selected Nvidia products becoming in stock.
+# The bot only functions in the UK region as the API works differently here.
+# Currently only support for "Founders Edition" graphics cards.
+# 
+# This is part of a repository hosted here: https://github.com/RicochetStudios/nvidia-bot
+# Created by: 
+# - https://github.com/kiweezi
+# - https://github.com/HazNut
+#
+
+
+
+# -- Imports --
+
+# -- End --
+
+
+
+# -- Global Variables --
+
+# -- End --
+
+
+
+# -- Main --
+
+# -- End --
